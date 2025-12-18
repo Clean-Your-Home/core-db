aab57e730bd0c8fd25e4cc1f986f1a9177d0df50
Varvara Zaitseva <karumasensey@gmail.com>
2025-12-18 21:05:09 +0300

    feat(tables): add reviews

---
+++ schemas/reviews/table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    booking_id UUID NOT NULL UNIQUE REFERENCES bookings(id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    rating INTEGER NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    comment VARCHAR(300),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
